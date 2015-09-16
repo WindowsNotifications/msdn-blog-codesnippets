10e39d097ab3d2600cdaaaeb8d353218543fbae4
anbare <anbare@microsoft.com>
2015-09-15 17:55:34 -0700

    displayName

---
+++ 10240/adaptive-tile-documentation/displayName/code.cs
@@ -0,0 +1,20 @@
+TileContent content = new TileContent()
+{
+    Visual = new TileVisual()
+    {
+        Branding = TileBranding.NameAndLogo,
+        DisplayName = "Wednesday 22",
+
+        TileMedium = new TileBinding()
+        {
+            DisplayName = "Wed. 22",
+            ...
+        },
+
+        // Inherits DisplayName from Visual
+        TileWide = new TileBinding()
+        {
+            ...
+        }
+    }
+};

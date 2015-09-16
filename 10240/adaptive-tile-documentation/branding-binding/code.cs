138a3646950b2ceb75e0a3cb8363a79a166ba81e
anbare <anbare@microsoft.com>
2015-09-15 17:51:52 -0700

    branding-binding

---
+++ 10240/adaptive-tile-documentation/branding-binding/code.cs
@@ -0,0 +1,19 @@
+TileContent content = new TileContent()
+{
+    Visual = new TileVisual()
+    {
+        Branding = TileBranding.NameAndLogo,
+
+        TileMedium = new TileBinding()
+        {
+            Branding = TileBranding.Logo,
+            ...
+        },
+
+        // Inherits branding from Visual
+        TileWide = new TileBinding()
+        {
+            ...
+        }
+    }
+};

913b3f2fa448c6c9fdf2b86546b936b626506df2
anbare <anbare@microsoft.com>
2015-09-22 11:29:10 -0700

    Added photos tile template code

---
+++ 10240/photos-tile-template/csharp.cs
@@ -0,0 +1,32 @@
+﻿/*
+
+To use the Photos template...
+
+ - On any TileBinding object
+   - Set Content property to new instance of TileBindingContentPhotos
+   - Add up to 12 images to Images property of TileBindingContentPhotos.
+
+*/
+
+TileContent content = new TileContent()
+{
+    Visual = new TileVisual()
+    {
+        TileMedium = new TileBinding()
+        {
+            Content = new TileBindingContentPhotos()
+            {
+                Images =
+                {
+                    new TileImageSource("Assets/1.jpg"),
+                    new TileImageSource("ms-appdata:///Images/2.jpg"),
+                    new TileImageSource("http://msn.com/images/3.jpg")
+
+                    // TODO: Can have 12 images total
+                }
+            }
+        }
+
+        // TODO: Add other tile sizes
+    }
+};

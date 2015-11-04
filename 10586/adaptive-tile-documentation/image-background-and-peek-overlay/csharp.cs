3594bf410926d2b42226e94d51d12720c990d0d0
anbare <anbare@microsoft.com>
2015-11-04 15:43:56 -0800

    Fixed peek-and-background overlay snippet

--- 10586/adaptive-tile-documentation/image-background-and-peek-overlay/csharp.cs
+++ 10586/adaptive-tile-documentation/image-background-and-peek-overlay/csharp.cs
@@ -7,13 +7,13 @@ TileMedium = new TileBinding()
         BackgroundImage = new TileBackgroundImage()
         {
             Source = new TileImageSource("Assets/Mostly Cloudy-Background.jpg"),
-            Overlay = 60
+            Overlay = 30
         },
         
         PeekImage = new TilePeekImage()
         {
             Source = new TileImageSource("Assets/Map.jpg"),
-            Overlay = 20
+            Overlay = 30
         },
 
         ...
